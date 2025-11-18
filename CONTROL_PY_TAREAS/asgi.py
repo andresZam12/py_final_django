6ea40308d485f907acae6f41797c102246625381
andresZam12 <giovanny.zambranob@campusucc.edu.co>
2025-11-18 13:40:28 -0500

    inicialización django creación de apps y config setting

---
+++ CONTROL_PY_TAREAS/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CONTROL_PY_TAREAS project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CONTROL_PY_TAREAS.settings')
+
+application = get_asgi_application()
