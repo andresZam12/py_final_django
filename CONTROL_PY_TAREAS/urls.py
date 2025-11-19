5fdb92f5335532c30efc6efff823d9f887836109
andresZam12 <giovanny.zambranob@campusucc.edu.co>
2025-11-19 18:01:36 -0500

    Agregar redireccion de raiz a panel

--- CONTROL_PY_TAREAS/urls.py
+++ CONTROL_PY_TAREAS/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/panel/', permanent=False)),
     path('admin/', admin.site.urls),
     path('cuentas/', include('cuentas.urls')),       # lo crearemos luego
     path('panel/', include('panel.urls')),           # dashboard
